25548f15e7a57b06e3631ac1a39a854464b3ce17
JonathasGit <jonathas-25@hotmail.com>
2022-04-05 23:04:59 -0300

    #3´exericios 1000 e 1001

---
+++ Respostas-Javascript/1001-Extremamente-Básico.js
@@ -0,0 +1,9 @@
+//var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+//var lines = input.split('\n');
+
+var a = prompt('NUmero 1 ');
+var b = prompt('numero2');
+var x = a + b;
+
+console.log( 'X =' + x);
+
---
+++ Respostas-Javascript/areaDoCirculo.js
@@ -0,0 +1,13 @@
+//var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+//var lines = input.split('\n');
+var n = 3.14159;
+var area = n * Math.pow(raio, 2);
+var raio = parseFloat(2);
+
+
+
+    
+console.log("A=" + area.toFixed(4));
+
+
+
